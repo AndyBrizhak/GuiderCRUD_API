c51aacbe6dc338670b4b49ea2a329d356f037684
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-10-14 10:54:37 +0800

    Create model Venue

--- GuiderCRUD_API/Models/Category.cs
+++ GuiderCRUD_API/Models/Category.cs
@@ -1,8 +1,19 @@
-﻿namespace GuiderCRUD_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuiderCRUD_API.Models
 {
     public class Category
     {
+        [Key] // Указывает на первичный ключ
+        public int Id { get; set; }
+
+        [Required] // Поле обязательно для заполнения
+        [MaxLength(100)] // Ограничение на максимальную длину строки
         public required string Name { get; set; }
+
         public string? Description { get; set; }
+
+        // Связь с таблицей Venue — одна категория имеет много заведений
+        public required ICollection<Venue> Venues { get; set; }
     }
 }
---
+++ GuiderCRUD_API/Models/Venue.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuiderCRUD_API.Models
+{
+    public class Venue
+    {
+        [Key] // Указывает на первичный ключ
+        public int Id { get; set; }
+
+        [Required] // Поле обязательно для заполнения
+        [MaxLength(200)] // Ограничение на максимальную длину строки
+        public required string Name { get; set; }
+
+        [Required] // Поле обязательно для заполнения
+        public required string Address { get; set; }
+
+        public string Descriprion { get; set; }
+
+        // Внешний ключ, указывающий на Category (Категория)
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        // Связь с моделью Category — каждое заведение относится к одной категории
+        public required Category Category { get; set; }
+
+        // Связь "многие ко многим" с тегами
+        
+    }
+}
