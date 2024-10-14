4145838e581d037165e89de249233fe22e7fdadf
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-10-14 11:25:35 +0800

    Add DbSet

---
+++ GuiderCRUD_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GuiderCRUD_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuiderCRUD_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Настройка отношения "многие ко многим" без явной промежуточной таблицы
+            modelBuilder.Entity<Venue>()
+                .HasMany(v => v.Tags)
+                .WithMany(t => t.Venues);
+        }
+    }
+}
